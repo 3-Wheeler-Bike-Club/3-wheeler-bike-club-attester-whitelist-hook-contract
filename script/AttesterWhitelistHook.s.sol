fed7bd0419514e1276ca90727d34753e341faa41
Tickether <tickether@gmail.com>
2025-03-22 22:59:13 +0000

    feat: remodel template for whitelist hook
    Base setup of contract dependencies + add attester whitelist hook & deploy script

--- script/AttesterWhitelistHook.s.sol
+++ script/AttesterWhitelistHook.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {AttesterWhitelistHook} from "../src/AttesterWhitelistHook.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract AttesterWhitelistHookScript is Script {
+    AttesterWhitelistHook public attesterWhitelistHook;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        attesterWhitelistHook = new AttesterWhitelistHook();
 
         vm.stopBroadcast();
     }
